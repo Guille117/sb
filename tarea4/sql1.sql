ca6ad6abe1d8d5aaf4bc8b3adf9cb4ae4462de95
Guille117 <130240186+Guille117@users.noreply.github.com>
2024-09-09 21:52:20 -0600

    Terminando base de datos

--- tarea4/sql1.sql
+++ tarea4/sql1.sql
@@ -1,1 +1,23 @@
+-- creando base de datos
 create database BibliotecaDB
+use BibliotecaDB
+
+-- creando tabla Libros
+
+create table Libros(
+ID_Libro int primary key identity,
+Titulo nvarchar(150) not null,
+Autor nvarchar(150) not null,
+Año_Publicacion int not null,
+Genero nvarchar(100),
+Disponibilidad bit not null
+);
+
+create table Miembros(
+ID_Miembro int primary key identity,
+Nombre nvarchar(70) not null,
+Apellido nvarchar(70) not null,
+Fecha_Registro Date not null,
+Tipo_Membresia varchar(10) not null
+);
+
